7e453be0529e42c840cd6d7ef31bf68e0ec14cb7
infeng <fzhihao@outlook.com>
2019-07-16 16:27:24 +0800

    fix: index.d.ts缺失部分导出

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,6 @@ export * from './lib/app';
 export { default as default } from './lib/app';
 export { useDispatch, useSelector } from 'react-redux';
 export * from './lib/hooks';
+export { default as connect } from './lib/connect';
+export { default as DynamicComponent } from './lib/DynamicComponent';
 export { default as createModel } from './lib/model';
